f04da5311f84145c958a65d7ee31ea50326f5058
nttlong <zugeliang2000@gmail.com>
2018-03-20 14:05:45 +0700

    save

--- nodejs/modules/ws/index.js
+++ nodejs/modules/ws/index.js
@@ -1,6 +1,62 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var request = require("request");
+var DEASYNC = require("deasync");
+exports.sync = function (fn, args, cb) {
+    function reject(e) {
+        result = {
+            error: e
+        };
+    }
+    ;
+    function resolve(r) {
+        result = {
+            result: r
+        };
+    }
+    if (args instanceof Array) {
+        var result = undefined;
+        var _cb = function (e, r) {
+            if (e) {
+                result = {
+                    error: e
+                };
+            }
+            else {
+                result = {
+                    result: r
+                };
+            }
+        };
+        args.push(_cb);
+        fn.apply(fn, args);
+        DEASYNC.loopWhile(function () {
+            return result === undefined;
+        });
+        if (result.error) {
+            throw (result.error);
+        }
+        else {
+            return result.result;
+        }
+    }
+    else {
+        var result = undefined;
+        fn({
+            resolve: resolve,
+            reject: reject,
+        });
+        DEASYNC.loopWhile(function () {
+            return result === undefined;
+        });
+        if (result.error) {
+            throw (result.error);
+        }
+        else {
+            return result.result;
+        }
+    }
+};
 var _url;
 function fn_error(cb, reject, ex) {
     if (cb)
@@ -42,4 +98,21 @@ function call(path, data, cb) {
     });
 }
 exports.call = call;
+function callSync(path, data) {
+    function run(cb) {
+        call(path, data).then(function (res) {
+            cb(null, res);
+        }).catch(function (ex) {
+            cb(ex);
+        });
+    }
+    try {
+        var ret = exports.sync(run, []);
+        return ret;
+    }
+    catch (ex) {
+        throw (ex);
+    }
+}
+exports.callSync = callSync;
 //# sourceMappingURL=index.js.map
--- nodejs/modules/ws/index.ts
+++ nodejs/modules/ws/index.ts
@@ -1,4 +1,67 @@
 import * as request from "request"
+import * as DEASYNC from "deasync";
+export var sync=<T>(fn:any, args:Array<any>, cb?:(error:any,ret:any)=>void):T=> {
+    function reject(e:any):void {
+
+        result = {
+            error: e
+        };
+    };
+    function resolve(r:any):void {
+        result = {
+            result: r
+        };
+    }
+    if (args instanceof Array) {
+        var result:any = undefined;
+        var _cb = function (e:any, r:any) {
+            if (e) {
+                result = {
+                    error: e
+                };
+
+
+            }
+            else {
+                result = {
+                    result: r
+                };
+
+            }
+
+
+        }
+        args.push(_cb);
+        fn.apply(fn, args);
+        DEASYNC.loopWhile(function () {
+            return result === undefined;
+        });
+        if (result.error) {
+            throw (result.error);
+        }
+        else {
+            return result.result;
+        }
+    }
+    else {
+        var result = undefined;
+        
+        fn({
+            resolve: resolve,
+            reject: reject,
+        });
+        DEASYNC.loopWhile(function () {
+            return result === undefined;
+        });
+        if (result.error) {
+            throw (result.error);
+        }
+        else {
+            return result.result;
+        }
+    }
+
+};
 var _url:string;
 function fn_error(cb,reject,ex){
     if(cb) cb(ex);
@@ -12,7 +75,7 @@ export function set_url(strUrl:string){
     _url=strUrl;
 }
 export function call(path:string,data?:any,cb?:(error?:any,result?:any)=>void):Promise<any>{
-    return new Promise((resolve,reject)=>{
+    return new Promise<any>((resolve,reject)=>{
         try {
             request.post(_url,{
                     json: true,
@@ -37,4 +100,20 @@ export function call(path:string,data?:any,cb?:(error?:any,result?:any)=>void):P
             
         }
     })
+}
+export function callSync(path:string,data?:any):any{
+    function run(cb){
+        call(path,data).then(res=>{
+            cb(null,res);
+        }).catch(ex=>{
+            cb(ex);
+        })
+    }
+    try {
+        var ret=sync(run,[]);
+        return ret;    
+    } catch (ex) {
+        throw(ex)
+    }
+    
 }
