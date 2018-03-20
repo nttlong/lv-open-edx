a9dbdb940c9bc2304f3350991ca2dcc46391c3b3
nttlong <zugeliang2000@gmail.com>
2018-03-20 14:06:07 +0700

    save

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,12 +1,12 @@
 var req =require("./modules/ws");
 req.set_url('http://0.0.0.0:8000/web_services/');
-// req.call("lv@get_list_of_students").then(res=>{
-//     console.log(res);
-// }).catch(ex=>{
-//     console.log(ex);
-// })
-try {
-    var ret=req.callSync("lv@get_list_of_students");    
-} catch (error) {
-    console.log("loi");
-}
+req.call("lv@get_list_of_students").then(res=>{
+    console.log(res);
+}).catch(ex=>{
+    console.log(ex);
+})
+// try {
+//     var ret=req.callSync("lv@get_list_of_students");    
+// } catch (error) {
+//     console.log("loi");
+// }
