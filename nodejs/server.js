350dfa2178a762c3865f487cad4b45bc43f75a7f
root <nguyenngocsang143@gmail.com>
2018-03-20 14:29:20 +0700

    edit file

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,8 @@
 var req =require("./modules/ws");
 req.set_url('http://0.0.0.0:8000/web_services/');
-req.call("lv@get_list_of_students").then(res=>{
+req.call("lv@get_user_db",{
+            username: "test1"
+        }).then(res=>{
     console.log(res);
 }).catch(ex=>{
     console.log(ex);
