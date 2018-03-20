9513ef823689b9ffd74fe9bd0da57cde8d147e96
nttlong <zugeliang2000@gmail.com>
2018-03-20 11:10:47 +0700

    save

--- lv/__init__.py
+++ lv/__init__.py
@@ -1,2 +1,4 @@
+import lv_utils
 def get_list_of_students():
+    lv_utils.mongo_db()
     return  ["a","b","c"]
---
+++ lv_utils/__init__.py
@@ -0,0 +1,33 @@
+import pymongo
+from pymongo import MongoClient
+from path import Path as path
+import os
+import json
+current_db=None
+def mongo_db():
+    global current_db
+    if(current_db==None):
+        PROJECT_ROOT = path(__file__).abspath().dirname().dirname()  # /edx-platform/cms
+        REPO_ROOT = PROJECT_ROOT.dirname()
+        ENV_ROOT = REPO_ROOT.dirname()
+        SERVICE_VARIANT = os.environ.get('SERVICE_VARIANT', None)
+        CONFIG_PREFIX = SERVICE_VARIANT + "." if SERVICE_VARIANT else ""
+        CONFIG_ROOT = path(os.environ.get('CONFIG_ROOT', ENV_ROOT))
+        with open(CONFIG_ROOT / CONFIG_PREFIX + "auth.json") as auth_file:
+            AUTH_TOKENS = json.load(auth_file)
+            # AUTH_TOKENS.get("DATABASES").get("default").update({"NAME": AUTH_TOKENS.get("MYSQL_CONFIG").get("NAME")})
+            # AUTH_TOKENS.get("DATABASES").get("default").update({"HOST": AUTH_TOKENS.get("MYSQL_CONFIG").get("HOST")})
+            # AUTH_TOKENS.get("DATABASES").get("default").update({"USER": AUTH_TOKENS.get("MYSQL_CONFIG").get("USER")})
+            # AUTH_TOKENS.get("DATABASES").get("default").update(
+            #     {"PASSWORD": AUTH_TOKENS.get("MYSQL_CONFIG").get("PASSWORD")})
+            # AUTH_TOKENS.get("DATABASES").get("default").update({"PORT": AUTH_TOKENS.get("MYSQL_CONFIG").get("PORT")})
+            client = MongoClient({
+                "host":AUTH_TOKENS.get("MONGODB_CONFIG").get("HOST"),
+                "port":AUTH_TOKENS.get("MONGODB_CONFIG").get("PORT"),
+                "user": AUTH_TOKENS.get("MONGODB_CONFIG").get("USER"),
+                "password": AUTH_TOKENS.get("MONGODB_CONFIG").get("PASSWORD")
+            })
+            current_db=client(AUTH_TOKENS.get("MONGODB_CONFIG").get("NAME"))
+
+    return current_db
+
